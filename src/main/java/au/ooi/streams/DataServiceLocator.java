0f5c1e8e5d34f04f0ce0324dee5eb12b97ffaf5a
arans <ayooi88@gmail.com>
2021-06-02 10:07:21 +1000

    Added working service locator test

--- src/main/java/au/ooi/streams/DataServiceLocator.java
+++ src/main/java/au/ooi/streams/DataServiceLocator.java
@@ -3,66 +3,76 @@ package au.ooi.streams;
 import org.zeromq.*;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
-public class DataServiceLocator {
-
-    private static final String IPC_SERVICE_LOCATOR = "ipc://./service-locator-ipc";
+public class DataServiceLocator implements Runnable {
 
     private final ZMQ.Socket socket;
 
     private final Map<String, Set<ServiceLocation>> map = new ConcurrentHashMap<>();
 
-    public DataServiceLocator(ZContext ctx) {
+    public DataServiceLocator(ZContext ctx, String serviceBindUrl) {
         socket = ctx.createSocket(SocketType.ROUTER);
-        socket.bind(IPC_SERVICE_LOCATOR);
+        socket.bind(serviceBindUrl);
+    }
+
+    void process() {
+        ZMsg msg = ZMsg.recvMsg(socket);
+        assert (msg.size() >= 3);
+        ZFrame address = msg.poll();
+
+        ZFrame req = msg.poll();
+        assert (req != null);
+
+        String requestStr = req.getString(ZMQ.CHARSET);
+        switch (requestStr) {
+            case "query" -> {
+                ZFrame data = msg.poll();
+                assert (data != null);
+                Set<ServiceLocation> serviceLocations = map.get(data.getString(ZMQ.CHARSET));
+                ZMsg result = new ZMsg();
+                result.add(address);
+                if (serviceLocations != null) {
+                    for (ServiceLocation serviceLocation : serviceLocations) {
+                        result.add(serviceLocation.getLocation());
+                    }
+                } else {
+                    result.add("Not Found");
+                }
+                result.send(socket);
+            }
+            case "register" -> {
+                ZFrame nameFrame = msg.poll();
+                assert (nameFrame != null);
+                String name = nameFrame.getString(ZMQ.CHARSET);
+
+                ZFrame locationFrame = msg.poll();
+                assert (locationFrame != null);
+                String location = locationFrame.getString(ZMQ.CHARSET);
+                this.map.putIfAbsent(name, new HashSet<>());
+                Set<ServiceLocation> serviceLocations = this.map.get(name);
+                if (serviceLocations != null) {
+                    serviceLocations.add(new ServiceLocation(location, Instant.now()));
+                    System.out.printf("[ServiceLocator] registered %s to %s%n", location, name);
+                }
+            }
+        }
+    }
+
+    public List<ServiceLocation> query(String serviceName) {
+        Set<ServiceLocation> serviceLocations = this.map.get(serviceName);
+        if (serviceLocations.isEmpty()) {
+            return Collections.emptyList();
+        } else {
+            return new ArrayList<>(serviceLocations);
+        }
     }
 
     public void run() {
         while (true) {
-            ZMsg msg = ZMsg.recvMsg(socket);
-            assert (msg.size() >= 3);
-            ZFrame address = msg.poll();
-
-            ZFrame req = msg.poll();
-            assert (req != null);
-
-            String requestStr = req.getString(ZMQ.CHARSET);
-            switch (requestStr) {
-                case "query" -> {
-                    ZFrame data = msg.poll();
-                    assert (data != null);
-                    Set<ServiceLocation> serviceLocations = map.get(data.getString(ZMQ.CHARSET));
-                    ZMsg result = new ZMsg();
-                    result.add(address);
-                    if (serviceLocations != null) {
-                        for (ServiceLocation serviceLocation : serviceLocations) {
-                            result.add(serviceLocation.getLocation());
-                        }
-                    } else {
-                        result.add("Not Found");
-                    }
-                    result.send(socket);
-                }
-                case "register" -> {
-                    ZFrame nameFrame = msg.poll();
-                    assert (nameFrame != null);
-                    String name = nameFrame.getString(ZMQ.CHARSET);
-
-                    ZFrame locationFrame = msg.poll();
-                    assert (locationFrame != null);
-                    String location = locationFrame.getString(ZMQ.CHARSET);
-                    this.map.putIfAbsent(name, new HashSet<>());
-                    Set<ServiceLocation> serviceLocations = this.map.get(name);
-                    if (serviceLocations != null) {
-                        serviceLocations.add(new ServiceLocation(location, Instant.now()));
-                        System.out.printf("[ServiceLocator] registered %s to %s%n", location, name);
-                    }
-                }
-            }
+            process();
         }
     }
 }
---
+++ src/test/java/au/ooi/streams/DataServiceLocatorTest.java
@@ -0,0 +1,53 @@
+package au.ooi.streams;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.zeromq.*;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class DataServiceLocatorTest {
+
+    private String serviceUrl = "inproc://service-location-url";
+    private String serviceName = "data-service";
+    private String dataUrl = "inproc://data-service-url";
+    private ZContext ctx = new ZContext();
+    private DataServiceLocator dataServiceLocator;
+
+    @Test
+    public void testRegister() {
+        dataServiceLocator = new DataServiceLocator(ctx, serviceUrl);
+
+        ZMQ.Socket socket = ctx.createSocket(SocketType.DEALER);
+        socket.connect(serviceUrl);
+
+        socket.sendMore("register");
+        socket.sendMore(serviceName);
+        socket.send(dataUrl);
+
+        dataServiceLocator.process();
+        List<ServiceLocation> query = dataServiceLocator.query(serviceName);
+        assertEquals(1, query.size());
+        assertEquals(dataUrl, query.get(0).getLocation());
+    }
+
+    @Test
+    public void testQuery() {
+        testRegister();
+
+        ZMQ.Socket dealer = ctx.createSocket(SocketType.DEALER);
+        dealer.connect(this.serviceUrl);
+        dealer.sendMore("query");
+        dealer.send(serviceName);
+        dataServiceLocator.process();
+
+        ZMsg zFrames = ZMsg.recvMsg(dealer);
+
+        ZFrame poll = zFrames.poll();
+        assertNotNull(poll);
+        assertEquals(dataUrl, poll.getString(ZMQ.CHARSET));
+    }
+}
